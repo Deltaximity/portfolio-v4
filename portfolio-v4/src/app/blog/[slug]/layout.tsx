6f9d6d683d8d67fe21bfcb74a35918ac0c5f1412
Deltaximity <hasali02@outlook.com>
2024-11-11 18:48:32 +0100

    move breadcrumbs location to blog page

--- portfolio-v4/src/app/blog/[slug]/layout.tsx
+++ portfolio-v4/src/app/blog/[slug]/layout.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Breadcrumbs from '@/components/Breadcrumbs';
 
 export default function BlogPostLayout({ children }: { children: React.ReactNode }) {
     return (
       <>
-        <Breadcrumbs />
         {children}
       </>
     )
